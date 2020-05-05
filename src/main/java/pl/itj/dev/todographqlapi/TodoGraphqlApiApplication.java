67fe863c8060da04ca0303e0c8af106563ef7f0c
Tomasz <tom.jopek@gmail.com>
2020-05-05 17:55:19 +0200

    [SONARQUBE] - sanitizing unused command line parameters

--- src/main/java/pl/itj/dev/todographqlapi/TodoGraphqlApiApplication.java
+++ src/main/java/pl/itj/dev/todographqlapi/TodoGraphqlApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodoGraphqlApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TodoGraphqlApiApplication.class, args);
+        SpringApplication.run(TodoGraphqlApiApplication.class);
     }
 
 }
